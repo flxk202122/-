df11b2b764f5a602e0b297683ff7c790ff16b23d
flxk202122 <flxk202122@naver.com>
2023-11-22 10:04:36 +0900

    데이터 분리 + json 작성

---
+++ data/navi.js
@@ -0,0 +1,30 @@
+const naviAlldata = {
+    gnb: [
+        {
+            nm: "네이버",
+            href: "http://www.naver.com"
+        },
+        {
+            // 엑셀 필드명
+            nm: "다음",
+            href: "http://www.daum.net"
+        }
+    ],
+    quick: [
+        {
+            nm: "깃",
+            href: "",
+            target: "_blank"
+        },
+        {
+            nm: "유튜브",
+            href: "",
+            target: "_blank"
+        },
+        {
+            nm: "페이스북",
+            href: "",
+            target: "_blank"
+        }
+    ],
+}
